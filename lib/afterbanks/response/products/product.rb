4afd709ede9185951e2a2245e9aa7790996b2927
Oriol Francès <oriolfa@gmail.com>
2016-06-09 12:03:24 +0200

    Add alias #product_number

--- lib/afterbanks/response/products/product.rb
+++ lib/afterbanks/response/products/product.rb
@@ -8,7 +8,10 @@ module Afterbanks
 
             instance_variable_set("@#{key}", value)
           end
+
+          singleton_class.class_eval { alias_method :product_number, :product }
         end
+
       end
     end
   end
